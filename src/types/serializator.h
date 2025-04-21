9623f485a74acdc6c839334c18ab8426e7fdf4a9
Daniel <d.akipskiy@gmail.com>
2025-04-21 19:57:52 +0800

    Добавьте файлы проекта.

---
+++ src/types/serializator.h
@@ -0,0 +1,11 @@
+class Serializator {
+public:
+    template<typename Arg>
+    void push(Arg&& _val);
+
+    Buffer serialize() const;
+
+    static std::vector<Any> deserialize(const Buffer& _val);
+
+    const std::vector<Any>& getStorage() const;
+};
